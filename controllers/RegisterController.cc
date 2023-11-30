316a747689c51571baa187d58b2265e643e474ab
fmartns <hello@fmartns.dev>
2023-11-30 09:14:57 -0300

    #8

--- controllers/RegisterController.cc
+++ controllers/RegisterController.cc
@@ -1,6 +1,20 @@
-#include "RegisterController.h"
+#include <drogon/HttpController.h>
+using namespace drogon;
 
-void RegisterController::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
-{
-    // write your application logic here
-}
+class RegisterController: public HttpController<RegisterController> {
+public:
+    METHOD_LIST_BEGIN
+    // use METHOD_ADD para adicionar seus métodos de requisição
+    METHOD_ADD(RegisterController::postRegister,"/register", Post);
+    METHOD_LIST_END
+
+    // Método para lidar com a requisição POST de registro
+    void postRegister(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) {
+        auto username = req->getParameter("username");
+        auto password = req->getParameter("password");
+        // Aqui você adiciona o código para inserir o usuário no banco de dados
+        auto resp=HttpResponse::newHttpResponse();
+        resp->setBody("Registro concluído!");
+        callback(resp);
+    }
+};
