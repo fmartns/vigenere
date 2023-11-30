d302f9bc9a052982fc7a4fd56ffd026a55e38886
fmartns <hello@fmartns.dev>
2023-11-30 08:30:19 -0300

    #6

--- main.cc
+++ main.cc
@@ -7,15 +7,8 @@ using namespace std;
 
 int main() {
 
-    //Load config file
     drogon::app().loadConfigFile("../config.json");
-
-    // Configuração dos controladores
-    drogon:app().registerController(LoginController());
-    drogon:app().registerController(RegisterController());
-
-    //drogon::app().loadConfigFile("../config.yaml");
-    //Run HTTP framework,the method will block in the internal event loop
     drogon::app().run();
     return 0;
+    
 }
