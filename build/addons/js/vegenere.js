e3edf4b2416d66116edd6833d84034776770df75
fmartns <hello@fmartns.dev>
2023-12-07 14:51:03 -0300

    #35

---
+++ build/addons/js/vegenere.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+    $("#submitBtn").click(function() {
+        var word = $("#word").val();
+
+        $.ajax({
+            url: '/vigenere',
+            type: 'POST',
+            data: { word: word },
+            success: function(response) {
+                $("#processedWord").text("Processed: " + response.processed);
+            },
+            error: function(xhr, status, error) {
+                $("#message").text("Erro ao descriptografar.");
+            }
+        });
+    });
+});
